7591f3a9ff40067f2fe3c9b783cf50d7a7dedcba
kruty121 <kruty121@users.noreply.github.com>
2026-07-04 02:59:49 +0300

    Update site links

--- src/data/links.ts
+++ src/data/links.ts
@@ -1,7 +1,7 @@
 export const links = {
   fanvue: "https://fanvue.com/REPLACE_ME",
   telegram: "https://t.me/REPLACE_ME",
-  twitter: "https://x.com/REPLACE_ME",
+  twitter: "https://x.com/sophiawonttext",
   instagram: "https://instagram.com/REPLACE_ME",
-  reddit: "https://reddit.com/user/REPLACE_ME",
+  reddit: "https://www.reddit.com/user/Sofia_lanexx/",
 } as const;
